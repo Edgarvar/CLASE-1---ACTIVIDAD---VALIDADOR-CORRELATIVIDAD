8b127ad2e5d1c203b8d31994476a69432a7a87d6
Edgarvar <edgardo_vargas@outlook.com.ar>
2023-11-07 02:35:45 -0300

    Trabajo terminado

--- src/main/java/validadorCorrelatividad/Materia.java
+++ src/main/java/validadorCorrelatividad/Materia.java
@@ -1,4 +1,33 @@
 package validadorCorrelatividad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Materia {
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nobre) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<Materia>();
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
+
+    public void agregarCorrelativa(Materia materia){
+        this.correlativas.add(materia);
+    }
+
+    public boolean puedeCursar(final Alumno alumno) {
+        return this.correlativas.stream().allMatch(materiaCorrelativa -> alumno.tenesCorrelativas(materiaCorrelativa));
+    }
+
+    public boolean tenesCorrelativas() {
+        return !this.correlativas.isEmpty();
+    }
 }
--- src/test/java/validadorCorrelatividad/InscripcionTest.java
+++ src/test/java/validadorCorrelatividad/InscripcionTest.java
@@ -1,4 +1,41 @@
 package validadorCorrelatividad;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InscripcionTest {
+    @Test
+    public void alumno1(){
+        Alumno alumno1 = new Alumno("Edgardo", "02020232");
+        Materia matematicaI = new Materia("Matematica I");
+        Inscripcion inscripcionEdgardo = new Inscripcion(alumno1, matematicaI);
+
+        Assertions.assertTrue(inscripcionEdgardo.aprobada());
+    }
+    @Test
+    public void alumno2(){ //No se debe poder inscribir a la materia
+        Alumno alumno2 = new Alumno("Brisa", "12312312");
+        Materia inglesI = new Materia("Ingles I");
+        Materia inglesII = new Materia("Ingles II");
+        inglesII.agregarCorrelativa(inglesI);
+
+        Inscripcion inscripcionBrisa = new Inscripcion(alumno2, inglesII);
+
+        Assertions.assertFalse(inscripcionBrisa.aprobada());
+    }
+
+    @Test
+    public void alumno3(){
+        Alumno alumno3 = new Alumno("Brisa", "12312312");
+        Materia inglesI = new Materia("Ingles I");
+        Materia inglesII = new Materia("Ingles II");
+
+        inglesII.agregarCorrelativa(inglesI);
+
+        alumno3.agregarMateriaAprobada(inglesI);
+
+        Inscripcion inscripcionBrisa = new Inscripcion(alumno3, inglesII);
+
+        Assertions.assertTrue(inscripcionBrisa.aprobada());
+    }
 }
